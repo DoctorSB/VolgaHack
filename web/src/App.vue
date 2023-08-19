ea085860899d93d30217f1f8429a0acba7026dac
tonitaga <onlyforcroyale@gmail.com>
2023-08-20 00:20:52 +0300

    template vue app file

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,55 +1,38 @@
 <template>
   <div class="app">
-    <form @submit.prevent>
-      <h4>Cоздание кластера</h4>
-      <input v-bind:value="title"
-             @input="title = $event.target.value"
-             class="input"
-             type="text"
-             placeholder="Название">
-      <input v-bind:value="body"
-             @input="body = $event.target.value"
-             class="input"
-             type="text"
-             placeholder="Описание">
-      <button class="button"
-              @click="createCluster">Создать
-      </button>
-    </form>
-
-    <div class="post" v-for="post in posts" :key="post.id">
-      <div><strong>Название</strong> {{post.title}}</div>
-      <div><strong>Описание</strong> {{post.body}}</div>
+    <div class="resident">
+      <div class="container" v-for="resident in residents" :key="resident.id">
+        <div><img :src=resident.image1path alt="{{resident.header}}"></div>
+        <div class="header-container"><strong>{{resident.header}}</strong></div>
+        <div class="container-info">{{resident.information}}</div>
+        <div><button class="container-button" @click="LoadInfoPage(resident.info_page_file)">Подробнее</button></div>
+      </div>
     </div>
-
   </div>
 </template>
 
-<script>
 
+<script>
 export default {
   data() {
     return {
-      posts: [],
-      title: '',
-      body: ''
+      residents: [
+        {id: 0, image1path: '/images/image1.jpg', header: 'Бар "Куйбышева"', information: 'Танцевальный бар с прекрасной волжской кухней и живыми концертами.', info_page_file: '1'},
+        {id: 1, image1path: '/images/image2.jpg', header: 'Монобар "Проходная', information: 'Монобар с говорящим названием, который перенесет тебя совершенно в другое место - проходную советского завода.', info_page_file: '2'},
+        {id: 2, image1path: '/images/image3.jpg', header: 'Ulandy Cafe', information: 'Еда на каждый день, которую удобно взять с собой! Наш гравный принцип - супер фреш.', info_page_file: '3'},
+        {id: 3, image1path: '/images/image4.jpg', header: 'Бистро "Зозо"', information: 'Современное ретро Битсро с открытой кухней.', info_page_file: '4'}
+      ],
     }
   },
   methods: {
-    createCluster() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body
-      }
-      this.posts.push(newPost)
-      this.title = ''
-      this.body = ''
+    LoadInfoPage(path) {
+      console.log(path)
     }
   }
 }
 
 </script>
+
 
 <style>
 * {
@@ -58,33 +41,53 @@ export default {
   box-sizing: border-box;
 }
 
-.app {
-  padding: 20px;
+.resident {
+  padding-top: 20px;
 }
 
-.post {
-  padding: 15px;
-  border: 2px solid seagreen;
-  margin-top: 15px;
+.container {
+  width: 75vw;
+  max-width: 100%;
+  margin: 0 auto;
+  padding-bottom: 50px;
 }
 
-form {
-  display: flex;
-  flex-direction: column;
-}
-
-.button {
-  margin-top: 15px;
-  align-self: flex-end;
-  padding: 10px 15px;
-  border: 2px solid seagreen;
-  background: none;
-}
-
-.input {
+.container img {
   width: 100%;
-  border: 2px solid seagreen;
-  padding: 10px 15px;
-  margin-top: 15px;
+  height: auto;
 }
+
+.header-container {
+  padding-top: 10px;
+  text-align: center;
+  text-decoration: underline;
+  text-decoration-color: red;
+  color: black;
+  font-size: 24px;
+  padding-bottom: 10px;
+}
+
+.container-info {
+  text-align: center;
+  font-size: 18px;
+  padding-bottom: 10px;
+}
+
+.container-button {
+  border: none;
+  background-color: transparent;
+  text-decoration: underline;
+  text-decoration-color: red;
+  color: black;
+  cursor: pointer;
+  font-size: 18px;
+  display: block;
+  margin: 0 auto;
+}
+
+.container-button:hover {
+  background-color: #934c50;
+  color: black;
+}
+
 </style>
