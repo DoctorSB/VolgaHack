5c3fd6d3c1b3670537b69f2a124605fd9cf3c511
Quicksilver Powder <quicksip@student.21-school.ru>
2023-08-20 03:29:09 +0300

    add web

---
+++ web/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app">
+    <residents></residents>
+  </div>
+</template>
+
+
+<script>
+import residents from "@/vue/residents.vue";
+
+export default {
+  components: {
+    residents
+  }
+}
+</script>
+
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
