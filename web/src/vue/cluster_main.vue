84184c611b7ac44a12d910183324fbcebd559b2f
tonitaga <onlyforcroyale@gmail.com>
2023-08-20 04:13:44 +0300

    Add second cluster for info

--- web/src/vue/cluster_main.vue
+++ web/src/vue/cluster_main.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="cluster">
       <div class="container" v-for="cluster in clusters" :key="cluster.id">
-        <div><img :src=cluster.image alt="{{cluster.title}}"></div>
         <div class="header-container"><strong>{{ cluster.title }}</strong></div>
+        <div><img :src=cluster.image alt="{{cluster.title}}"></div>
         <div class="container-info">{{ cluster.info }}</div>
+        <div class="container-info"> Типо график </div>
         <div><button class="container-button" @click="LoadInfoPage(cluster.resident_path)">Подробнее</button></div>
+
       </div>
     </div>
 </template>
